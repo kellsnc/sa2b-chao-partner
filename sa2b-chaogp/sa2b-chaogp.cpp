ffba8aa6d933d00bf1991f7017f3fb02d2a5cfb9
Kell <kellspam@outlook.fr>
2020-05-02 12:16:45 +0200

    Initial commit

---
+++ sa2b-chaogp/sa2b-chaogp.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+
+extern "C"
+{
+	__declspec(dllexport) void Init(const char* path) {
+		
+	}
+
+	__declspec(dllexport) void OnFrame() {
+		
+	}
+
+	__declspec(dllexport) ModInfo SA2ModInfo = { ModLoaderVer };
+}
